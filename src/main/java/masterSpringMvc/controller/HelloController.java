7c5345f5a67d7bc1c3cd0aec21bd1f08355807af
liilith93 <urszmol393@student.polsl.pl>
2018-03-08 22:18:40 +0100

    Dodanie parameter name w linku

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -2,14 +2,21 @@ package masterSpringMvc.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+//@RequestMapping("/")
+//public String hello(Model model) {
+//    model.addAttribute("message", "Witaj w kontrolerze!");
+//    return "resultPage";
+//}
 @RequestMapping("/")
-@ResponseBody
-public String hello() {
-    return "Witaj, świecie!";
+public String hello(@RequestParam(defaultValue = "świecie", required = false) String name, Model model) {
+    model.addAttribute("message", "Witaj, " + name + "!");
+    return "resultPage";
 }
 }
